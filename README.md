af5d84b307d15aa2504cebc23d378458714d8bb8
Amit Upadhyay <amitupadhyay10287@gmail.com>
2019-02-24 11:18:20 +0700

    Added all files with complete setup

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# omise
-Omise Tamboon React
+# Omise Code Challenges
+
+This repository contains small donation platform where user can donate the suitable amount of their choice.
+
+### How it works
+
+* npm install
+* npm run server - For server to serve api to fetch all donations
+* npm run client - It serves the browser to view platform where user can start with donations
+
+### How it serves
+* http://localhost:3000/ - For client service
+* http://localhost:3001/ - For server service
---
+++ webpack.config.js
@@ -0,0 +1,43 @@
+const path = require('path');
+require('babel-polyfill');
+
+const config = {
+  entry: ['babel-polyfill','./src/Layout.js'],
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: '[name].js',
+    publicPath: 'build'
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    inline: true,
+    host: '0.0.0.0',
+    port: 3000,
+    historyApiFallback: true,
+    disableHostCheck: true,
+    contentBase: 'public',
+  },
+  module: {
+    rules: [
+      {
+        test: [/\.jsx?$/, /\.js?$/],
+        exclude: /(node_modules)/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['react', 'es2015'],
+          plugins: ['transform-class-properties']
+        },
+      },
+      {
+        test: [/\.bmp$/, /\.gif$/, /\.jpe?g$/, /\.png$/],
+        loader: require.resolve('url-loader'),
+        options: {
+          limit: 10000,
+          name: 'static/media/[name].[hash:8].[ext]',
+        },
+      },
+    ],
+  },
+};
+
+module.exports = config;
